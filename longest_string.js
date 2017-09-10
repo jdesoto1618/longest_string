ba60b3b3cba521f8c25e699df3b2db1b362a24d6
Joe DeSoto <modulusman@gmail.com>
2017-09-10 09:03:55 -0700

    Initial commit, made js file, wrote parent function, added test for empty array and array of length 1

---
+++ longest_string.js
@@ -0,0 +1,9 @@
+function find_longest (arr) {
+  //program determines the longest string in a given array
+  //check if the array is empty, or has just one element
+  if (arr.length === 0) {
+    return false;
+  } else if (arr.length === 1) {
+    return arr;
+  }
+}
